e386a3567c7184136806e7d94e815a797d47e264
Nicolò Vignali <153986252+NicoVigna@users.noreply.github.com>
2026-01-23 07:36:01 +0000

    creazione main

---
+++ src/main/java/com/battaglianavale/Main.java
@@ -0,0 +1,8 @@
+package com.battaglianavale;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Battaglia Navale - Avvio del gioco");
+        
+    }
+}
