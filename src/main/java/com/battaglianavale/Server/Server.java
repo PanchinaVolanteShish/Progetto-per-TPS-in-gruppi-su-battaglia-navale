a6893cf44e7258673f9bafaf2d29d671202ec7d8
Marcusilmitico2007 <153986442+Marcusilmitico2007@users.noreply.github.com>
2026-01-23 09:27:25 +0000

    Server

--- src/main/java/com/battaglianavale/Server/Server.java
+++ src/main/java/com/battaglianavale/Server/Server.java
@@ -1,5 +1,26 @@
 package com.battaglianavale.Server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    
+    public static void main(String[] args) {
+        int port = 5000;
+        Gioco gioco = new Gioco();
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server avviato sulla porta " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Nuovo client connesso");
+
+                ClientThread thread = new Thread(socket, gioco);
+                thread.start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
