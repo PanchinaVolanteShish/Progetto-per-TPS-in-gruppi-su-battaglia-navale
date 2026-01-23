0e164c1e3d0ef9156da0c0b0c98ceeccdff9c091
Noorlyn <a.prodigo.lobo@gmail.com>
2026-01-23 09:43:32 +0100

    inizializzazione client

--- src/main/java/com/battaglianavale/Client/Client.java
+++ src/main/java/com/battaglianavale/Client/Client.java
@@ -1,6 +1,22 @@
 package com.battaglianavale.Client;
 
+import java.io.BufferedReader;
+import java.net.Socket;
+
 public class Client {
-    
+
+    public static void main (String[] args)
+    {
+        String host = "localhost";
+        int porta = 5003;
+        Gson gson = new Gson();
+
+        try (Socket socket = new Socket(host,porta))
+        {
+            PrintWriter out = new PrinterWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new inputStreamReader(socket.getInputStream()));
+            
+        }
+    }
     
 }
