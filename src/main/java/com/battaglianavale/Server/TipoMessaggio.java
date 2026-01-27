e2214269fdeb0b21e858e6c143bb0f79cbdad4e3
Marcusilmitico2007 <153986442+Marcusilmitico2007@users.noreply.github.com>
2026-01-27 15:00:07 +0000

    modifiche

---
+++ src/main/java/com/battaglianavale/Server/TipoMessaggio.java
@@ -0,0 +1,16 @@
+package com.battaglianavale.Server;
+
+public enum TipoMessaggio {
+    JOIN,
+    JOIN_OK,
+    GAME_START,
+    PLACE_SHIPS,
+    PLACE_SHIPS_OK,
+    ATTACK,
+    ATTACK_RESULT,
+    INCOMING_ATTACK,
+    TURN_CHANGE,
+    ERROR,
+    GAME_OVER
+
+}
